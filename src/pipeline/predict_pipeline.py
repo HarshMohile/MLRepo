8680c426f89423869308e30b9601e1cc5869a1e7
HarshMohile <hsmohile@gmail.com>
2023-04-12 18:10:06 +0530

    flask app done with ebs aws

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,3 +1,5 @@
+
+import os
 import sys
 import pandas as pd
 from src.exception import CustomException
@@ -11,7 +13,7 @@ class PredictPipeline:
     def predict(self,features):
         try:
             model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
@@ -29,7 +31,7 @@ class CustomData:
     def __init__(  self,
         gender: str,
         race_ethnicity: str,
-        parental_level_of_education,
+        parental_level_of_education:str,
         lunch: str,
         test_preparation_course: str,
         reading_score: int,
